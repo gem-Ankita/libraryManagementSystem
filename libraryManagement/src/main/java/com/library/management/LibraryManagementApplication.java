0ee1429e1efbb3d324ed2460c895af48a1e4d537
gem-Ankita <ankita.verma@geminisolutions.com>
2022-12-05 19:42:22 +0530

    swagger

--- libraryManagement/src/main/java/com/library/management/LibraryManagementApplication.java
+++ libraryManagement/src/main/java/com/library/management/LibraryManagementApplication.java
@@ -1,11 +1,14 @@
 package com.library.management;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "CRUD Application " ,version = "2.7.1" , description = "Library Management Using Spring Boot") )
 public class LibraryManagementApplication {
 
 	public static void main(String[] args) {
